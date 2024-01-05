ea3e2a342477686e0a8e011d21397903f5578b04
mkzero <mkzero@mkzerodeMacBook-Air.local>
2024-01-06 07:47:16 +0800

    add file

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-set -U fish_greeting
-    /opt/homebrew/bin/brew shellenv | source 
-     starship init fish | source
-     alias vim=nvim
-     alias ls=lsd
-     navi widget fish | source
+    set -U fish_greeting
+    if test -x /opt/homebrew/bin/brew
+        /opt/homebrew/bin/brew shellenv | source
+    end
+    starship init fish | source
+    alias vim=nvim
+    alias ls=lsd
+    navi widget fish | source
 end
 
