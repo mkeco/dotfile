459fc7a7c90716dbdfe4292fc343a3e7b91444dc
mkzero <mkzero@mkzerodeAir.lan>
2024-02-01 22:30:38 +0800

    add commond

---
+++ cloud-nixos/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 添加 NixOS unstable channel
+nix-channel --add https://mirrors.cernet.edu.cn/nix-channels/nixos-unstable nixos
+
+# 更新 channel
+nix-channel --update
+
+# 为磁盘创建 GPT 分区表
+parted /dev/vda -- mklabel gpt 
+
+# 创建根分区,从 512MB 到磁盘末尾
+parted /dev/vda -- mkpart root ext4 512MB 100%
+
+# 创建 512MB 的 EFI 系统分区 
+parted /dev/vda -- mkpart ESP fat32 1MB 512MB
+
+# 设置第2分区为 EFI system 分区
+parted /dev/vda -- set 2 esp on
+
+# 给根分区打上文件系统标签 nixos
+mkfs.ext4 -L nixos /dev/vda1
+
+# 给 EFI 分区打上文件系统标签 boot
+mkfs.fat -F 32 -n boot /dev/vda2
+
+# 挂载根分区到 /mnt
+mount /dev/disk/by-label/nixos /mnt  
+
+# 创建 /mnt/boot 目录
+mkdir -p /mnt/boot 
+
+# 挂载 EFI 分区到 /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
