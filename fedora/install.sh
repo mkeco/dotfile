ac5439907edbe20740133330408d6e01662b4cda
mkzero <mkzeroagem@gmail.com>
2024-02-02 19:45:12 +0800

    add commond

---
+++ fedora/install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo dnf install fish go navi lsd neovim conda curl weget -y
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh 
+
+sudo usermod -aG docker $USER
+
+echo "修改docker镜像🇨🇳."
+
+sudo systemctl start docker
+
+sudo sh -c 'cat > /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": [
+    "https://docker.mirrors.sjtug.sjtu.edu.cn",
+    "https://hub-mirror.c.163.com"
+  ]
+}
+EOF'
+
+sudo systemctl restart docker
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+curl -fsSL https://bun.sh/install | bash
