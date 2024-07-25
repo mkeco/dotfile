87e28de26acb0659ca10ad587f9d354f00db1f28
mkzero <mkzeroagem@gmail.com>
2024-07-25 08:52:05 +0800

    fix

---
+++ fedora/vm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 切换 shell    
+sudo dnf install zsh -y 
+chsh -s /bin/zsh
+echo "source ~/.config/fedora/.zshrc" >> ~/.zshrc
+
+# 安装必备工具    
+sudo dnf install curl btop unzip navi helix zoxide exa bat  -y
+
+
+# 安装提示符 
+curl -sS https://starship.rs/install.sh | sh
+
+
+# 安装 语言 
+sudo dnf install go -y
+
+
