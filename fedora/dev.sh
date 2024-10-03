ab9fd0ab45c6ac614f684f81e8eb0b63f5f45df8
Super User <root@localhost.localdomain>
2024-10-03 22:09:14 +0000

    fix

---
+++ fedora/dev.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 切换 shell    
+sudo dnf install zsh -y 
+chsh -s /bin/zsh
+echo "source ~/.config/fedora/.zshrc" >> .zshrc
+
+# 安装必备工具    
+sudo dnf install curl btop unzip wget navi helix  -y
+
+# 安装 docker
+sudo sh get-docker.sh 
+
+sudo usermod -aG docker $USER
+
+echo "修改docker镜像🇨🇳."
+
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo sh -c 'cat > /etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": [
+    "https://docker.mkzeroagem.workers.dev"
+  ]
+}
+EOF'
+
+sudo systemctl restart docker
+# 安装提示符 
+curl -sS https://starship.rs/install.sh | sh
+
+
+# 安装 语言 
+sudo dnf install go -y
+
+
